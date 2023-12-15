5ca8ab2d84dac1c6c3e9abc85fdc0bb5927a94eb
Ubed Khan <ubedkhan2383@gmail.com>
2023-12-15 14:35:59 +0530

    fsa

--- js/script.js
+++ js/script.js
@@ -10,14 +10,16 @@
  */
 
 
-
+var develop = document.getElementById('develop')
 var icons = document.getElementById("icons")
 icons.onclick = function (){
     document.body.classList.toggle("dark-theme")
     if(document.body.classList.contains('dark-theme')){
         icons.src = 'img/sun.png'
+        develop.src = 'img/DEVLOPFOLIO.jpg'
     }else{
         icons.src = 'img/moon.png'
+        develop.src = 'img/DEVLOPFOLIO.png'
         
     }
 }
