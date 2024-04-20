20ef54cf4fd23347078a285387d15c915fb9d510
Ubed2003 <147146015+Ubed2003@users.noreply.github.com>
2024-04-20 19:52:44 +0530

    as

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 
 var develop = document.getElementById('develop')
 var icons = document.getElementById("icons")
-icons.onclick = function (){
+icons.addEventListener( 'click', function (){
     document.body.classList.toggle("dark-theme")
     if(document.body.classList.contains('dark-theme')){
-        icons.src = 'img/sun.png'
+        icons.innerHTML =  'a'
+        icons.style.color = 'white'
         develop.src = 'img/DEVLOPFOLIO.jpg'
     }else{
-        icons.src = 'img/moon.png'
+        icons.innerHTML =  'chal'
+        icons.style.color = 'black'
         develop.src = 'img/DEVLOPFOLIO.png'
         
     }
-}
+})
 
 // var icons = document.getElementById("icons")
 // icons.onclick = function (){
